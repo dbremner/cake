6bb4e1dc13034233ca5b726899561d81de81f707
David Bremner <dbremner@gmail.com>
2018-05-30 23:14:24 -0700

    remove unneeded scope

--- cake.c
+++ cake.c
@@ -25,21 +25,19 @@ int PASCAL WinMain(HANDLE hInstance, HANDLE hPrevInstance, LPSTR lpszCmdLine, in
 
     hInst = hInstance;
 
-    {
-        wndclass.style = CS_HREDRAW | CS_VREDRAW;
-        wndclass.lpfnWndProc = WndProc;
-        wndclass.cbClsExtra = 0;
-        wndclass.cbWndExtra = 0;
-        wndclass.hInstance = hInstance;
-        wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
-        wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
-        wndclass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
-        wndclass.lpszMenuName = szAppName;
-        wndclass.lpszClassName = szAppName;
+    wndclass.style = CS_HREDRAW | CS_VREDRAW;
+    wndclass.lpfnWndProc = WndProc;
+    wndclass.cbClsExtra = 0;
+    wndclass.cbWndExtra = 0;
+    wndclass.hInstance = hInstance;
+    wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wndclass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+    wndclass.lpszMenuName = szAppName;
+    wndclass.lpszClassName = szAppName;
 
-        if (!RegisterClass(&wndclass))
-            return FALSE;
-    }
+    if (!RegisterClass(&wndclass))
+        return FALSE;
 
     hwnd = CreateWindow(szAppName, "Birthday Cake",
         WS_TILEDWINDOW, 0, 0, 0, 0,
